9fc4174ecc2d6eda836f1e8b6937e371b404c99d
Graham Booth <graham.r.booth@gmail.com>
2013-11-01 02:36:06 +0000

    finally managed to add functions to detect TCP connection establishment and termination

--- packet-analyser.py
+++ packet-analyser.py
@@ -39,7 +39,8 @@ from impacket.ImpactDecoder import EthDecoder, LinuxSLLDecoder
 
 class DecoderThread(Thread):
     def __init__(self, pcapObj):
-        handshakes = {}
+        self.handshakes = {}
+        self.endshakes = {}
         # OSC functionality:
         sendAddress = '127.0.0.1', 57120
         self.oscClient=OSCClient()
@@ -52,7 +53,6 @@ class DecoderThread(Thread):
             self.decoder = LinuxSLLDecoder()
         else:
             raise Exception("Datalink type not supported: " % datalink)
-
         self.pcap = pcapObj
         Thread.__init__(self)
 
@@ -60,13 +60,6 @@ class DecoderThread(Thread):
         # Sniff ad infinitum.
         # PacketHandler shall be invoked by pcap for every packet.
         self.pcap.loop(0, self.packetHandler)
-
-    def packetHandler(self, hdr, data):
-        p = self.decoder.decode(data)
-        ip = p.child()
-        tcp = ip.child()
-       
-        return (p,ip,tcp,src,dst)
             
     def packetHandler(self, hdr, data):
         # Use the ImpactDecoder to turn the rawpacket into a hierarchy
@@ -83,34 +76,89 @@ class DecoderThread(Thread):
             src = (ip.get_ip_src(), tcp.get_th_sport() )
             dst = (ip.get_ip_dst(), tcp.get_th_dport() )
             self.detectStart(tcp, src, dst)
+            self.detectEnd(tcp, src, dst)
 
-    def detectStart(thread, tcp, src, dst): # when passed, tcp grows into two arguments the first of which is a thread, why?!
-        # stolen from: https://github.com/larrytheliquid/buffer-overflows/blob/master/project-2/project-2-submission/main.py
-        # Handshake 1
-        if tcp.get_th_flags() == TH_SYN:
-            client_server = (src,dst)
-            self.handshakes[client_server] = { "client_seq" : tcp.get_th_seq() }
-        # Handshake 2
-        elif tcp.get_th_flags() == TH_SYN | TH_ACK:
-            client_server = (dst,src)
-            if client_server in self.handshakes:
-                hs = self.handshakes[client_server]
-                if hs.get("client_seq",None) == tcp.get_th_ack() - 1:
-                    hs["server_seq"] = tcp.get_th_seq()
-        # Handshake 3
-        elif tcp.get_th_flags() == TH_ACK:
-            client_server = (src,dst)
-            if client_server in self.handshakes:
-                hs = self.handshakes[client_server]
-                if hs.get("client_seq",None) == tcp.get_th_seq() - 1 and \
-                   hs.get("server_seq",None) == tcp.get_th_ack() - 1:
-                    self.handshakes.pop(client_server)
-                    sendStart(src, dst)
+    def detectStart(self, tcp, src, dst):
+        # modified from: https://github.com/larrytheliquid/buffer-overflows/blob/master/project-2/project-2-submission/main.py
 
-    def sendStart(src, dst):
-        print "succesful handshake"
-        print (src, dst)
+        # 02:29:04.696934 IP (tos 0x0, ttl 64, id 50453, offset 0, flags [DF], proto TCP (6), length 64)
+        # 10.0.0.69.63660 > 64.4.11.42.80: Flags [S], cksum 0x3e5a (correct), seq 1634890365, win 65535
+        # 02:29:04.875864 IP (tos 0x0, ttl 242, id 17512, offset 0, flags [DF], proto TCP (6), length 52)
+        # 64.4.11.42.80 > 10.0.0.69.63660: Flags [S.], cksum 0x7468 (correct), seq 1876445385, ack 1634890366, win 8190
+        # 02:29:04.876027 IP (tos 0x0, ttl 64, id 49239, offset 0, flags [DF], proto TCP (6), length 40)
+        # 10.0.0.69.63660 > 64.4.11.42.80: Flags [.], cksum 0x94d1 (correct), seq 1634890366, ack 1876445386, win 16384, length 0
+        # 02:29:04.876151 IP (tos 0x0, ttl 64, id 16719, offset 0, flags [DF], proto TCP (6), length 158)
+        
+        # SYN: The active open is performed by the client sending a SYN to the server.
+        # The client sets the segment's sequence number to a random value A.
+        # SYN-ACK: In response, the server replies with a SYN-ACK.
+        # The acknowledgment number is set to one more than the received sequence number i.e. A+1
+        # ...and the sequence number that the server chooses for the packet is another random number, B.
+        # ACK: Finally, the client sends an ACK back to the server.
+        # The sequence number is set to the received acknowledgement value i.e. A+1,
+        # and the acknowledgement number is set to one more than the received sequence number i.e. B+1.
 
+        # Csam replies with a similar packet except it includes a piggy-backed ack for rtsg's SYN. Rtsg then acks csam's SYN. The `.' means the ACK flag was set. The packet contained no data so there is no data sequence number. Note that the ack sequence number is a small integer (1). The first time tcpdump sees a tcp `conversation', it prints the sequence number from the packet. On subsequent packets of the conversation, the difference between the current packet's sequence number and this initial sequence number is printed. This means that sequence numbers after the first can be interpreted as relative byte positions in the conversation's data stream (with the first data byte each direction being `1'). `-S' will override this feature, causing the original sequence numbers to be output.        
+
+        if (tcp.get_SYN() == 1) & (tcp.get_ACK() == 0): # request: SYN without ACK
+            cs = (src,dst)            
+            if cs not in self.handshakes:
+                self.handshakes[cs] = { "client_seq" : tcp.get_th_seq() }
+
+        elif (tcp.get_SYN() == 1) & (tcp.get_ACK() == 1): # request: SYN with ACK        
+            cs = (dst,src)
+            if cs in self.handshakes:
+                hs = self.handshakes[cs]
+                if (hs.get("client_seq", None)) == (tcp.get_th_ack() - 1):
+                    hs["server_seq"] = tcp.get_th_seq()                    
+                    hs["server_ack"] = tcp.get_th_ack()
+
+        if (tcp.get_SYN() == 0) & (tcp.get_ACK() == 1): # acknowledgement: ACK without SYN
+            cs = (src,dst)
+            if cs in self.handshakes:
+                hs = self.handshakes[cs]
+                if (hs["server_seq"] == tcp.get_th_ack() - 1) & (hs["server_ack"] == tcp.get_th_seq()):
+                    hs = self.handshakes[cs]
+                    self.handshakes.pop(cs)
+                    self.sendStart(src, dst)
+
+    def sendStart(self, src, dst):  
+        print "connection established between:" + str((src, dst))
+
+    def detectEnd(self, tcp, src, dst):
+        # 02:29:41.824045 IP (tos 0x0, ttl 64, id 62228, offset 0, flags [DF], proto TCP (6), length 40)
+        # 10.0.0.69.63661 > 64.4.11.42.80: Flags [F.], cksum 0xfc20 (correct), seq 3708332116, ack 2779445817, win 16384, length 0
+        # 02:29:42.002991 IP (tos 0x0, ttl 242, id 43373, offset 0, flags [DF], proto TCP (6), length 40)
+        # 64.4.11.42.80 > 10.0.0.69.63661: Flags [F.], cksum 0x3a21 (correct), seq 2779445817, ack 3708332117, win 511, length 0
+        # 02:29:42.003103 IP (tos 0x0, ttl 64, id 63350, offset 0, flags [DF], proto TCP (6), length 40)
+        # 10.0.0.69.63661 > 64.4.11.42.80: Flags [.], cksum 0xfc1f (correct), seq 3708332117, ack 2779445818, win 16384, length 0        
+        
+        if (tcp.get_FIN() == 1) & (tcp.get_ACK() == 1):
+            cs = (src,dst)
+            if cs not in self.endshakes: # request
+                self.endshakes[cs] = { "client_seq" : tcp.get_th_seq(), "client_ack" : tcp.get_th_ack() }
+                
+        if (tcp.get_FIN() == 1) & (tcp.get_ACK() == 1):
+            cs = (dst,src)           
+            if cs in self.endshakes: # respond
+                es = self.endshakes[cs]
+                if es.get("client_seq",None) == tcp.get_th_ack() - 1 and \
+                  es.get("client_ack",None) == tcp.get_th_seq():
+                    es["server_seq"] = tcp.get_th_seq()
+                    es["server_ack"] = tcp.get_th_ack()
+
+        if (tcp.get_FIN() == 0) & (tcp.get_ACK() == 1):
+            cs = (src,dst)
+            if cs in self.endshakes: # acknowledge
+                es = self.endshakes[cs]
+                if es.get("server_seq",None) == tcp.get_th_ack() - 1 and \
+                  es.get("server_ack",None) == tcp.get_th_seq():
+                    self.endshakes.pop(cs)
+                    self.sendEnd(src, dst)
+
+    def sendEnd(self, src, dst):  
+        print "connection terminated between:" + str((src, dst))
+ 
 class HTTPRequest(BaseHTTPRequestHandler):
     def __init__(self, request_text):
         self.rfile = StringIO(request_text)
